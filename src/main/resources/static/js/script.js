84404db2cf4752736a846dd41cb1a47176563b20
Borislav Kolomiets <bbarra641@gmail.com>
2018-06-13 11:09:31 +0300

    migration commit

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,21 +1,57 @@
 $(document).ready(function(){
-
-    $('.add-product-btn').on('click', function () {
-       // if ($("[name = 'productName']").val() === '') {
-           $('#exampleModal3').arcticmodal({
-               closeOnEsc: false,
-               closeOnOverlayClick: false,
-               overlay: {
-                   css: {
-                       backgroundColor: '#fff',
-                       backgroundImage: 'url(images/overlay.png)',
-                       backgroundRepeat: 'repeat',
-                       backgroundPosition: '50% 0',
-                       opacity: .75
-                   }
-               }
-           });
-       // }
-    });
+    requiredAddProduct();
 });
+
+function requiredAddProduct() {
+    $('.add-product-btn').on('click', function () {
+        isEmptyPrice();
+        // isEmptyWeight();
+        // isEmptyCategory();
+    });
+}
+
+function isEmptyPrice() {
+    var priceS = $("[name = 'priceS']").val();
+    var priceM = $("[name = 'priceM']").val();
+    var priceL = $("[name = 'priceL']").val();
+
+    if (priceS === '' && priceM === '' && priceL === '') {
+        $('.add-product-btn').attr('data-toggle', 'modal');
+        $('.add-product-btn').attr('data-target', '.bd-example-modal-sm');
+        $('.add-product-btn').attr('type', 'button');
+        $('.modal-content-p').text('Price is not specified!');
+    } else {
+        $('.add-product-btn').attr('data-toggle', '');
+        $('.add-product-btn').attr('data-target', '');
+        $('.add-product-btn').attr('type', 'submit');
+    }
+}
+
+function isEmptyWeight() {
+    var weightS = $("[name = 'weightS']").val();
+    var weightM = $("[name = 'weightM']").val();
+    var weightL = $("[name = 'weightL']").val();
+
+    if (weightS === '' && weightM === '' && weightL === '') {
+        $('.add-product-btn').attr('data-toggle', 'modal');
+        $('.add-product-btn').attr('data-target', '.bd-example-modal-sm');
+        $('.add-product-btn').attr('type', 'button');
+        $('.modal-content-p').text('Weight is not specified!');
+    } else {
+        $('.add-product-btn').attr('data-toggle', '');
+        $('.add-product-btn').attr('data-target', '');
+        $('.add-product-btn').attr('type', 'submit');
+    }
+}
+
+function isEmptyCategory() {
+    if ($("[name = 'category']").val() === 'Choice...') {
+        $('.add-product-btn').attr('data-toggle', 'modal');
+        $('.add-product-btn').attr('data-target', '.bd-example-modal-sm');
+        $('.add-product-btn').attr('type', 'button');
+        $('.modal-content-p').text('Choose a category of the product.');
+    } else {
+        $('.add-product-btn').attr('type', 'submit');
+    }
+}
 
